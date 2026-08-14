3a69935a5a5cc4c0b29beb014dc0fd5b04415b4d
Junior2704 <louisdabadie9@gmail.com>
2026-08-14 08:55:33 +0200

    sdefr

--- messagerie-envoi.js
+++ messagerie-envoi.js
@@ -32,7 +32,7 @@ const firebaseConfig = {
 const app = getApps().length
   ? getApp()
   : initializeApp(firebaseConfig);
-
+const db = getFirestore(app); 
 
 
 /* ==================================================================
